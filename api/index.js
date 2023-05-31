f0cc98a3cb495bd311b7aee3e7ab33424864bd84
EnzoMagurno <magurnoenzo31602@gmail.com>
2023-05-31 12:20:25 -0300

    changes into PORT index

--- api/index.js
+++ api/index.js
@@ -19,11 +19,10 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-require('dotenv').config()
-const { PORT } = process.env
-console.log(PORT);
+
 conn.sync({ force: false }).then(() => {
-  server.listen(PORT, () => {
-    console.log('%s listening at ', PORT);
+  server.listen(3001, () => {
+    console.log('%s listening at ', 3001);
   });
 });
+
